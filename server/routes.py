1db7a27954ecf992de60c77230dfd5066b1be613
Harmeet Goindi <goindi@gmail.com>
2021-02-17 15:09:54 -0500

    normalized prob

--- server/routes.py
+++ server/routes.py
@@ -13,17 +13,23 @@ async def get_range_data(symbol: str, days:int = 7) -> dict:
     return get_range_data_from_symbol(symbol,days)
 
 @router.get("/yolo/{my_list}")
-async def get_my_yolo(my_list: str) -> dict:
+async def get_my_yolo(my_list: str, days:int = 7, sigma:float = 0.5) -> dict:
     symbol_list = my_list.split(",")
     yolo_dict = {}
-    max_std = 0
-    max_std_symbol = ""
+    max_up_prob = 0
+    max_up_symbol = ""
+    max_down_prob = 0
+    max_down_symbol = ""
     for i in symbol_list:
-        yolo_dict[i] = get_range_data_from_symbol(i,7)
-        if yolo_dict[i]["ivol"] > max_std:
-            max_std = yolo_dict[i]["ivol"]
-            max_std_symbol = i
-    return yolo_dict[max_std_symbol]
+        print(i)
+        yolo_dict[i] = get_normalized_probability_move(i, days, sigma)
+        if yolo_dict[i]["norm_prob_up"] > max_up_prob:
+            max_up_prob = yolo_dict[i]["norm_prob_up"]
+            max_up_symbol = i
+        if yolo_dict[i]["norm_prob_down"] > max_down_prob:
+            max_down_prob = yolo_dict[i]["norm_prob_down"]
+            max_down_symbol = i
+    return {"bullish_stock_symbol":max_up_symbol, "bullish_stock_details":yolo_dict[max_up_symbol], "bearish_stock_symbol":max_down_symbol, "bearish_stock":yolo_dict[max_down_symbol]}
     #return yolo_dict
 
 @router.get("/call_trades/{symbol}")
