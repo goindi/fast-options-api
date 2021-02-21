e1d0004eae3ad9570ba9a23564726728400c358e
Harmeet Goindi <goindi@gmail.com>
2021-02-21 02:00:54 +0000

    added cors policy

--- server/api.py
+++ server/api.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from server.routes import router as OptionRouter
 app = FastAPI()
+
+origins = [
+        "http://localhost.tiangolo.com",
+        "https://localhost.tiangolo.com",
+        "http://localhost",
+        "http://localhost:8080",
+          ]
+
+app.add_middleware(
+         CORSMiddleware,
+         allow_origins=["*"],
+         allow_credentials=True,
+         allow_methods=["*"],
+         allow_headers=["*"],
+         )
 
 @app.get("/", tags=["Root"])
 async def read_root():
