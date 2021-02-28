ef8ef81a78acd05cf1b720913c41e9a3e90af218
Harmeet Goindi <goindi@gmail.com>
2021-02-28 16:52:13 -0500

    cache

--- server/calc_module.py
+++ server/calc_module.py
@@ -6,9 +6,16 @@ from scipy import stats
 import yfinance as yf
 import trading_calendars as tc
 import pandas as pd
+from scipy.stats import norm
+import ast
+CACHE_TIMEOUT = 120
 
-def get_expiries_bracket(ticker, num_of_days):
-    c = Call(ticker)
+
+import redis
+r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+
+def get_expiries_bracket(symbol, num_of_days):
+    c = Call(symbol)
     expiries = c.expirations
     curr_date = str(datetime.date(datetime.now()))
     longer_expiry = expiries[-1]
@@ -31,8 +38,8 @@ def get_expiries_bracket(ticker, num_of_days):
         shorter_weight = (longer_day_bound - num_of_days) / (longer_day_bound - shorter_day_bound)
     return {'shorter_expiry':shorter_expiry,'longer_expiry':longer_expiry,'shorter_day_bound':shorter_day_bound,'longer_day_bound':longer_day_bound, 'shorter_weight':shorter_weight}
 
-def get_strike_bracket(call_object, price_target):
-    strikes = call_object.strikes
+def get_strike_bracket(strikes, price_target):
+    #strikes = call_object.strikes
     higher_strike = strikes[0]
     lower_strike = strikes[0]
     if price_target < strikes[0]:
@@ -56,7 +63,7 @@ def get_delta(symbol:str, percent_move:float, expiry:str):
     up_px = s.price*(1+percent_move/100)
     down_px = s.price*(1-percent_move/100)
     call = Call(symbol,d=int(expiry[0:2]),m=int(expiry[3:5]),y=int(expiry[6:10]))
-    up_delta_dict = get_strike_bracket(call, up_px)
+    up_delta_dict = get_strike_bracket(call.strikes, up_px)
     call.set_strike(up_delta_dict['lower_strike'])
     delta1 = call.delta()*up_delta_dict['lower_weight']
     call.set_strike(up_delta_dict['higher_strike'])
@@ -64,11 +71,61 @@ def get_delta(symbol:str, percent_move:float, expiry:str):
     delta_up_move = delta1 + delta2
 
     put = Put(symbol,d=int(expiry[0:2]),m=int(expiry[3:5]),y=int(expiry[6:10]))
-    down_delta_dict = get_strike_bracket(put, down_px)
+    down_delta_dict = get_strike_bracket(put.strikes, down_px)
     put.set_strike(down_delta_dict['lower_strike'])
     delta1 = -put.delta()*down_delta_dict['lower_weight']
     put.set_strike(down_delta_dict['higher_strike'])
     delta2 = -put.delta()*(1-down_delta_dict['lower_weight'])
+    delta_down_move = delta1 + delta2
+    return {'delta_up':delta_up_move,'delta_down':delta_down_move}
+
+def get_nd2(symbol:str, percent_move:float, expiry:str):
+    y = yf.Ticker(symbol)
+    info = y.info
+    price = (info['bid']+info['ask'])/2
+    up_px = price*(1+percent_move/100)
+    down_px = price*(1-percent_move/100)
+    expiry = f'{expiry[6:10]}-{expiry[3:5]}-{expiry[0:2]}'
+    o = y.option_chain(date='2021-03-12')
+    up_delta_dict = get_strike_bracket(o.calls.strike.tolist(), up_px)
+    x = o.calls
+    x['q'] = info['dividendYield']
+    x['r']=0.001
+    x['t'] = 8/365
+    x['s'] = price
+    x['d1']= (np.log(x.s/x.strike)+x.t*(x.r-x.q+x.impliedVolatility*x.impliedVolatility/2))/(x.impliedVolatility*np.sqrt(x.t))
+    #x['nd1'] = norm.cdf(x.d1)
+    x['d2'] = x.d1-x.impliedVolatility*np.sqrt(x.t)
+    x['nd2'] = norm.cdf(x.d2)
+
+
+
+    #call.set_strike(up_delta_dict['lower_strike'])
+    delta1 = (x.loc[x['strike'] == up_delta_dict['lower_strike']]['nd2'])*up_delta_dict['lower_weight']
+    #call.set_strike(up_delta_dict['higher_strike'])
+    delta2 = (x.loc[x['strike'] == up_delta_dict['higher_strike']]['nd2'])*(1-up_delta_dict['lower_weight'])
+    #delta2 = call.delta()*(1-up_delta_dict['lower_weight'])
+    delta_up_move = delta1 + delta2
+
+    #put = Put(symbol,d=int(expiry[0:2]),m=int(expiry[3:5]),y=int(expiry[6:10]))
+    down_delta_dict = get_strike_bracket(o.puts.strike.tolist(), down_px)
+
+    y = o.puts
+    y['q'] = info['dividendYield']
+    y['r']=0.001
+    y['t'] = 8/365
+    y['s'] = price
+    y['d1']= (np.log(y.s/y.strike)+y.t*(y.r-y.q+y.impliedVolatility*y.impliedVolatility/2))/(y.impliedVolatility*np.sqrt(y.t))
+    #x['nd1'] = norm.cdf(x.d1)
+    y['d2'] = y.d1-y.impliedVolatility*np.sqrt(y.t)
+    y['nd2'] = norm.cdf(y.d2)
+    #put.set_strike(down_delta_dict['lower_strike'])
+    #delta1 = -put.delta()*down_delta_dict['lower_weight']
+    delta1 = (y.loc[y['strike'] == down_delta_dict['lower_strike']]['nd2'])*down_delta_dict['lower_weight']
+
+    #put.set_strike(down_delta_dict['higher_strike'])
+    #delta2 = -put.delta()*(1-down_delta_dict['lower_weight'])
+    delta2 = (y.loc[y['strike'] == down_delta_dict['higher_strike']]['nd2'])*(1-down_delta_dict['lower_weight'])
     delta_down_move = delta1 + delta2
     return {'delta_up':delta_up_move,'delta_down':delta_down_move}
 
@@ -79,7 +136,7 @@ def get_atm_ivol(s, ndays=30):
     #First Shorter One
     x = expiry_dict['shorter_expiry']
     shorter_call = Call(symbol,d=int(x[0:2]),m=int(x[3:5]),y=int(x[6:10]))
-    strike_dict = get_strike_bracket(shorter_call, s.price)
+    strike_dict = get_strike_bracket(shorter_call.strikes, s.price)
     shorter_call.set_strike(strike_dict['lower_strike'])
     lower_vol = shorter_call.implied_volatility()
     shorter_call.set_strike(strike_dict['higher_strike'])
@@ -88,7 +145,7 @@ def get_atm_ivol(s, ndays=30):
     #Now longer One
     x = expiry_dict['longer_expiry']
     longer_call = Call(symbol,d=int(x[0:2]),m=int(x[3:5]),y=int(x[6:10]))
-    strike_dict = get_strike_bracket(longer_call, s.price)
+    strike_dict = get_strike_bracket(longer_call.strikes, s.price)
     longer_call.set_strike(strike_dict['lower_strike'])
     lower_vol = longer_call.implied_volatility()
     longer_call.set_strike(strike_dict['higher_strike'])
@@ -99,10 +156,14 @@ def get_atm_ivol(s, ndays=30):
     return (implied_ivol, one_sigma_move_ndays_day)
 
 def range_data_from_symbol(symbol, ndays=7, sigma=1.15):
+    symbol = symbol.lower()
     return_dict = {"symbol": "Error",
                     "desc": "No Data found for %s"%symbol
                 }
     try:
+        if r.hget(f'{symbol}|range|{ndays}|{sigma}','time') and (int(datetime.utcnow().strftime('%s')) - int(r.hget(f'{symbol}|range|{ndays}|{sigma}','time'))) < CACHE_TIMEOUT:
+            return ast.literal_eval(r.hget(f'{symbol}|range|{ndays}|{sigma}','value'))
+        r.hset(f'{symbol}|range|{ndays}|{sigma}','time',datetime.utcnow().strftime('%s'))
         s = Stock(symbol)
         my_tuple = get_atm_ivol(s, ndays)
         volume = stock_volume(symbol, ndays)
@@ -117,8 +178,10 @@ def range_data_from_symbol(symbol, ndays=7, sigma=1.15):
         return_dict["today_volume"] = volume["volume"]
         return_dict["avg_10d_volume"] = volume["avg_10d_volume"]
         return_dict["prob_up"] = prob["prob_up"]
-
+        r.hset(f'{symbol}|range|{ndays}|{sigma}','time',datetime.utcnow().strftime('%s'))
+        r.hset(f'{symbol}|range|{ndays}|{sigma}','value',str(return_dict))
         return return_dict
+
     except:
         return return_dict
 
@@ -140,9 +203,12 @@ def calculate_bollingers(s, ndays = 20, sigma = 2):
 def kelly_fraction(win_prob : float, win_loss_ratio:float)->float:
     return win_prob - (1-win_prob)/win_loss_ratio
 
-def best_call_trades(ticker, num_of_days):
-    c = Call(ticker)
-    range_dict = range_data_from_symbol(ticker, num_of_days)
+def best_call_trades(symbol, num_of_days):
+    symbol=symbol.lower()
+    if r.hget(f'{symbol}|calltrade|{num_of_days}','time') and (int(datetime.utcnow().strftime('%s')) - int(r.hget(f'{symbol}|calltrade|{num_of_days}','time'))) < CACHE_TIMEOUT:
+        return ast.literal_eval(r.hget(f'{symbol}|calltrade|{num_of_days}','value'))
+    c = Call(symbol)
+    range_dict = range_data_from_symbol(symbol, num_of_days)
     curr_date = str(datetime.date(datetime.now()))
     expiries = c.expirations
     expiry_to_use = expiries[0]
@@ -151,7 +217,7 @@ def best_call_trades(ticker, num_of_days):
         expiry_to_use = i
         if days_to_exp >= num_of_days:
             break
-    c = Call(ticker,d=int(expiry_to_use[0:2]),m=int(expiry_to_use[3:5]),y=int(expiry_to_use[6:10]))
+    c = Call(symbol,d=int(expiry_to_use[0:2]),m=int(expiry_to_use[3:5]),y=int(expiry_to_use[6:10]))
     counter = 0
     spread_list = []
     strikes = c.strikes
@@ -183,8 +249,10 @@ def best_call_trades(ticker, num_of_days):
                     max_amt = win_amt
                     best_spread = {'strike_to_sell':i['strike'],'strike_to_buy':j['strike'], 'premium_received':i['bid'], 'premium_paid':j['ask'], 'expiry':expiry_to_use}
     best_call_written['expiry'] = expiry_to_use
-
-    return {'best_spread':best_spread,'best_call':best_call_written}
+    return_dict = {'best_spread':best_spread,'best_call':best_call_written}
+    r.hset(f'{symbol}|calltrade|{num_of_days}','time',datetime.utcnow().strftime('%s'))
+    r.hset(f'{symbol}|calltrade|{num_of_days}','value',str(return_dict))
+    return return_dict
 
 def prob_move_pct(symbol:str, n_days:int, percent:float):
     c = Call(symbol)
@@ -241,7 +309,7 @@ def implied_forward(symbol, n_days):
             break
     call = Call(symbol,d=int(expiry_to_use[0:2]),m=int(expiry_to_use[3:5]),y=int(expiry_to_use[6:10]))
     put = Put(symbol,d=int(expiry_to_use[0:2]),m=int(expiry_to_use[3:5]),y=int(expiry_to_use[6:10]))
-    bracket_dict = get_strike_bracket(call, s.price)
+    bracket_dict = get_strike_bracket(call.strikes, s.price)
     forward = s.price
     if bracket_dict['lower_weight'] > 0.5:
         call.set_strike(bracket_dict['lower_strike'])
@@ -253,9 +321,13 @@ def implied_forward(symbol, n_days):
         forward = bracket_dict['higher_strike'] - put.price + call.price
     return {"forward_price":forward,"current_price":s.price, "expiry":expiry_to_use}
 
-def best_put_trades(ticker, num_of_days):
-    p = Put(ticker)
-    range_dict = range_data_from_symbol(ticker, num_of_days)
+def best_put_trades(symbol, num_of_days):
+    symbol=symbol.lower()
+    if r.hget(f'{symbol}|puttrade|{num_of_days}','time') and (int(datetime.utcnow().strftime('%s')) - int(r.hget(f'{symbol}|puttrade|{num_of_days}','time'))) < CACHE_TIMEOUT:
+        return ast.literal_eval(r.hget(f'{symbol}|puttrade|{num_of_days}','value'))
+
+    p = Put(symbol)
+    range_dict = range_data_from_symbol(symbol, num_of_days)
     curr_date = str(datetime.date(datetime.now()))
     expiries = p.expirations
     expiry_to_use = expiries[0]
@@ -264,7 +336,7 @@ def best_put_trades(ticker, num_of_days):
         expiry_to_use = i
         if days_to_exp >= num_of_days:
             break
-    p = Put(ticker,d=int(expiry_to_use[0:2]),m=int(expiry_to_use[3:5]),y=int(expiry_to_use[6:10]))
+    p = Put(symbol,d=int(expiry_to_use[0:2]),m=int(expiry_to_use[3:5]),y=int(expiry_to_use[6:10]))
     counter = 0
     spread_list = []
     strikes = p.strikes
@@ -296,14 +368,24 @@ def best_put_trades(ticker, num_of_days):
                     max_amt = win_amt
                     best_spread = {'strike_long':i['strike'],'strike_short':j['strike'], 'premium_received':i['bid'], 'premium_paid':j['ask'], 'expiry':expiry_to_use}
     best_put_written['expiry'] = expiry_to_use
-    return {'best_spread':best_spread,'best_put':best_put_written}
+    return_dict = {'best_spread':best_spread,'best_put':best_put_written}
+    r.hset(f'{symbol}|puttrade|{num_of_days}','time',datetime.utcnow().strftime('%s'))
+    r.hset(f'{symbol}|puttrade|{num_of_days}','value',str(return_dict))
+    return return_dict
+
 
 def amt_to_invest(symbol:str,n_days:int):
+    symbol=symbol.lower()
+    if r.hget(f'{symbol}|kelly|{days}','time') and (int(datetime.utcnow().strftime('%s')) - int(r.hget(f'{symbol}|kelly|{days}','time'))) < CACHE_TIMEOUT:
+        return ast.literal_eval(r.hget(f'{symbol}|kelly|{days}','value'))
     prob_dict = prob_move_pct(symbol, n_days,0)
     #print(prob_dict)
     curr_date = str(datetime.date(datetime.now()))
     days_to_exp = abs(datetime.strptime(prob_dict['expiry'],'%d-%m-%Y') - datetime.strptime(curr_date,'%Y-%m-%d')).days
-    return {"kelly":2*prob_dict['prob_up'] - 1, "expiry":prob_dict['expiry'], "prob_up":prob_dict['prob_up'], "kelly2":prob_dict['prob_up']-0.5}
+    return_dict = {"kelly":2*prob_dict['prob_up'] - 1, "expiry":prob_dict['expiry'], "prob_up":prob_dict['prob_up'], "kelly2":prob_dict['prob_up']-0.5}
+    r.hset(f'{symbol}|kelly|{days}','time',datetime.utcnow().strftime('%s'))
+    r.hset(f'{symbol}|kelly|{days}','value',str(return_dict))
+    return return_dict
 
     # my_tuple = get_atm_ivol(Stock(symbol), days_to_exp)
     # perc_move = my_tuple[1]*1.15/2
@@ -311,6 +393,7 @@ def amt_to_invest(symbol:str,n_days:int):
     # return (prob_dict['prob_up'] - prob_dict['prob_down'])/perc_move
 
 def div_details(symbol:str):
+    symbol = symbol.upper()
     y = yf.Ticker(symbol)
     if y.dividends.empty:
         div = "0"
@@ -323,18 +406,35 @@ def div_details(symbol:str):
     return {'div':div, 'div_date':div_date, 'div_yld':div_yld}
 
 def stock_volume (symbol:str, n_days:int):
-    xnys = tc.get_calendar("XNYS")
+    symbol = symbol.upper()
+    curr_date = str(datetime.now().date())
+    if not r.get(curr_date):
+        xnys = tc.get_calendar("XNYS")
+        print("here")
+        if xnys.is_session(pd.Timestamp(datetime.now())):
+            r.set(curr_date,"trading")
+        else:
+            r.set(curr_date,"not-trading")
     s = yf.Ticker(symbol)
     weight = 1
-    if xnys.is_session(pd.Timestamp(datetime.now())):
-        d1 = datetime.now()
+    d1 = datetime.now()
+    #if d1.hour>9 and d1.hour<16:
+
+    if r.get(curr_date) == "trading":
+        print("here2")
         d2 = d1.replace(hour=9)
         d2 = d1.replace(minute=30)
         delta = d1 - d2
-        weight = (390*60)/delta.total_seconds()
-        p = stats.percentileofscore(s.history()[-n_days:].Volume,s.info['volume']*weight)
-    else:
-        p = stats.percentileofscore(s.history()[-n_days:].Volume,s.info['volume'])
-    today_volume = s.info['volume']*weight
+        if delta.total_seconds() >= 390*60:
+            weight = 1
+            r.set(curr_date,"not-trading")
+        else:
+            weight = (390*60)/delta.total_seconds()
 
-    return {'symbol':symbol, 'percentile':p, 'volume':today_volume, 'avg_10d_volume':s.info['averageVolume10days']}
+    today_volume = s.info['volume']*weight
+    p = stats.percentileofscore(s.history()[-n_days:].Volume,today_volume)
+    avg_10d_volume = 0
+    if r.hget(symbol,'date') != curr_date or not r.hget(symbol,'avg_10d_volume') :
+        r.hset(symbol,'date', curr_date)
+        r.hset(symbol,'avg_10d_volume', s.info['averageVolume10days'])
+    return {'symbol':symbol, 'percentile':p, 'volume':today_volume, 'avg_10d_volume':r.hget(symbol,'avg_10d_volume')}
